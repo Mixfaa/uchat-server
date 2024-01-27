382698fad8c529a75e1afa2ad23996767cedceda
Миша Език <mishunder9354@gmail.com>
2024-01-28 00:21:31 +0200

    maybe_this_commit_broke_all

--- src/main/kotlin/ua/mezik/uchat/socket/SocketClient.kt
+++ src/main/kotlin/ua/mezik/uchat/socket/SocketClient.kt
@@ -11,6 +11,7 @@ import ua.mezik.uchat.model.message.requests.TransactionBase
 import ua.mezik.uchat.services.ChatClient
 import ua.mezik.uchat.services.HeartbeatSender
 import ua.mezik.uchat.services.TransactionsResolver
+import java.io.InputStream
 import java.net.Socket
 import java.util.concurrent.atomic.AtomicBoolean
 
@@ -20,44 +21,37 @@ class SocketClient(
     private val clientSocket: Socket,
     private val heartbeatSender: HeartbeatSender,
     private val transactionsResolver: TransactionsResolver,
-) : ChatClient {
+) : ChatClient, AutoCloseable {
     private var isConnected = AtomicBoolean(true)
 
     override fun sendToClient(transaction: TransactionBase) {
         if (clientSocket.isClosed) return
         clientSocket.getOutputStream().write(transaction.serialized)
     }
+
     override fun sendToClient(serializedTransaction: SerializedTransaction) {
         if (clientSocket.isClosed) return
         clientSocket.getOutputStream().write(serializedTransaction)
     }
 
-    private fun heartbeatFallback(ex: Exception) {
-        isConnected.set(false)
-        transactionsResolver.clientDisconnected(this)
-        clientSocket.close()
-
-        heartbeatSender.removeReceiver(this)
-    }
 
     fun handleAsync() {
-        heartbeatSender.addReceiver(this, ::heartbeatFallback)
+        heartbeatSender.addReceiver(this) { _ -> close() }
 
         coroutineContext.launch {
             val inputStream = clientSocket.getInputStream()
             while (isConnected.get()) {
                 try {
                     var availableToRead = inputStream.available()
-                    while (availableToRead <= 0) {
+                    
+                    while (availableToRead <= 0 && isConnected.get()) {
                         availableToRead = inputStream.available()
                         Thread.sleep(50)
                     }
 
-                    if (!isConnected.get()) return@launch
+                    val message = String(inputStream.readNBytes(availableToRead))
 
-                    val rawMessage = String(inputStream.readNBytes(availableToRead))
-                    println("message: $rawMessage message end;")
-                    for (json in Utils.splitJsons(rawMessage)) {
+                    for (json in Utils.splitJsons(message)) {
                         val probablyRequest = Utils.jsonMapper.readValue<TransactionBase>(json)
                         println(probablyRequest)
 
@@ -66,11 +60,70 @@ class SocketClient(
 
                         sendToClient(response)
                     }
+
                 } catch (ex: Exception) {
                     ex.printStackTrace()
+                    close()
                     return@launch
                 }
             }
         }
     }
+
+    override fun close() {
+        isConnected.set(false)
+        transactionsResolver.clientDisconnected(this)
+        clientSocket.close()
+
+        heartbeatSender.removeReceiver(this)
+    }
 }
+
+
+
+/*
+    fun handleAsync() {
+        heartbeatSender.addReceiver(this) { _ -> close() }
+
+        coroutineContext.launch {
+            val inputStream = clientSocket.getInputStream()
+            val messageBuilder = StringBuilder()
+            while (isConnected.get()) {
+                try {
+                    if (!isConnected.get()) return@launch
+
+                    do {
+                        messageBuilder.append(readFromSocket(inputStream))
+                    } while (messageBuilder.lastOrNull() != '\n' || messageBuilder.contains('\n')) // we will wait and read from socket until we get complete message
+
+                    val lastIndexOfDelimiter = messageBuilder.lastIndexOf('\n')
+
+                    val completeMessage: String
+
+                    if (messageBuilder.lastIndex == lastIndexOfDelimiter) {
+                        completeMessage = messageBuilder.toString()
+                        messageBuilder.clear()
+                    } else {
+                        completeMessage = messageBuilder.substring(0, lastIndexOfDelimiter + 1)
+                        messageBuilder.deleteRange(0, lastIndexOfDelimiter + 1) // test it
+                    }
+
+                    for (json in Utils.splitJsons(completeMessage)) {
+                        val probablyRequest = Utils.jsonMapper.readValue<TransactionBase>(json)
+                        println(probablyRequest)
+
+                        val response = transactionsResolver.handleRequest(probablyRequest, this@SocketClient)
+                            ?: continue
+
+                        sendToClient(response)
+                    }
+
+                } catch (ex: Exception) {
+                    ex.printStackTrace()
+                    close()
+                    return@launch
+                }
+            }
+        }
+    }
+ */
